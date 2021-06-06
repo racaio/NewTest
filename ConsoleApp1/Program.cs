c021f3cda9fb6427d91b0038eb72ad2a276667e1
racaio <akaiorafael@gmail.com>
2021-06-06 10:15:18 -0300

    novotesteDoGit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 
 namespace ConsoleApp1
 {
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New Teste Git");
         }
     }
 }
